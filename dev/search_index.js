52aded9b0c6a399442cd7f9763c8aae45675c599
Documenter.jl <documenter@juliadocs.github.io>
2022-01-10 08:07:02 +0000

    build based on e102dd9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sesam","category":"page"},{"location":"#Sesam","page":"Home","title":"Sesam","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sesam.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sesam]","category":"page"}]
+}
