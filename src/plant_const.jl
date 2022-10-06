41b7fae6da0090cc2736d663612ba6292f37ed7a
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2022-10-06 16:25:40 +0200

    add u_Pup parameter to plant_const

--- src/plant_const.jl
+++ src/plant_const.jl
@@ -7,6 +7,7 @@ Need to specify parameters
 optional:
 - `u_PlantNmax0`, `u_PlantPmax0`: maximum plant uptake of inorganic N and P
 - `k_PlantN0`, `k_PlantP0`: plant uptake rate per inorganic pool of N and P 
+- `u_Pup`: plant inorganic P uplift (to potentially modify `u_PlantPmax0`)
 
 Defaults of plant uptake are set so that uptake rate is very high (1e3) so that
 the maximum is taken up, i.e. wins competition with microbes.
@@ -23,9 +24,10 @@ function plant_const(;name)
     # end) 
     @parameters t 
     D = Differential(t)
-    @parameters i_L0  i_IN0   
-    @parameters u_PlantNmax0  k_PlantN0 β_Ni0 i_IP0  β_Pi0 u_PlantPmax0  k_PlantP0 s_EP0
-    @variables (begin
+    ps = @parameters (i_L0,  i_IN0,   
+        u_PlantNmax0, u_Pup, 
+        k_PlantN0, β_Ni0, i_IP0, β_Pi0, u_PlantPmax0, k_PlantP0, s_EP0)
+    states = @variables (begin
         i_L(t), 
         β_Ni(t), i_IN(t),
         u_PlantNmax(t), k_PlantN(t),
@@ -47,17 +49,19 @@ function plant_const(;name)
     ]
     defaults=Pair{Num, Any}[
         # rate not constrained, to make u_PlantNmax0 effective
-        k_PlantN0 => 1e3, 
+        k_PlantN0 => 1.0e3, 
         # take as much N as provided with litter
         u_PlantNmax0 => i_L0/β_Ni0, 
         # rate not constrained
-        k_PlantP0 => 1e3, 
+        k_PlantP0 => 1.0e3, 
         # take as much P as provided with litter
         u_PlantPmax0 => i_L0/β_Pi0, 
         # defaults in order to work with the CN-only version
         β_Pi0 => 50.0, i_IP0 => 0.0, s_EP0 => 0.0,
+        # uplift of inorganic P from deeper soil
+        u_Pup => 0.0,
     ]
-    ODESystem(eqs; name, defaults)    
+    ODESystem(eqs, t, states, ps; name, defaults)    
 end
 
 
